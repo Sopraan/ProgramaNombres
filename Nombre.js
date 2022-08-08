310998047c739c453aee50b1c059ab57c01b1ca1
Zeti <sopraaan@gmail.com>
2022-08-08 07:44:00 -0300

    vueta de proyecto a la notebook

--- Nombre.js
+++ Nombre.js
@@ -8,8 +8,10 @@ fs.readdir(PATH, (error, araydeNombresDeLosArchivos) => {
   } else {
     console.log(araydeNombresDeLosArchivos)
 
+    araydeNombresDeLosArchivos = araydeNombresDeLosArchivos.sort();
+
     renombrar(araydeNombresDeLosArchivos);
-    
+
   }
 });
 
@@ -20,17 +22,56 @@ function cambiarElNombre(nombre) {
   for (let i = 0; i < 3; i++) {
     nombreCortado += recorte[i] + "_";
   }
-  return `${nombreCortado}.pdf`;
+  return `${nombreCortado}`;
 }
 
-function renombrar(nombres) {
-  nombres.forEach((cadaNombre) => {
-    fs.rename(PATH + cadaNombre, PATH + cambiarElNombre(cadaNombre), (err) => {
-      if (err) {
-        throw err;
-      }
+/*function renombrar(nombres) {
+  let nombresCambiados = [];
 
-      console.log("Rename complete!");
-    });
+  nombres.forEach((cadaNombre) => {
+    let nuevoNombre = cambiarElNombre(cadaNombre);
+    let busqueda = nombresCambiados.find(archivo => archivo.nombre === nuevoNombre);
+    if (busqueda) {
+      busqueda.cantidad++;
+      renameNode(cadaNombre,(`${nuevoNombre}_${busqueda.cantidad}.pdf`))  
+    } else {
+      let archivo = {
+        nombre: nuevoNombre,
+        cantidad: 1
+      }
+      nombresCambiados.push(archivo)
+      renameNode(cadaNombre,(`${nuevoNombre}_1.pdf`))
+      
+    }
   });
 }
+
+*/
+function renombrar(nombre){
+  cambiarElNombre(nombre)
+nombreCortado.forEach( nombreConNumero=> {
+  let temporalNombreConNumero = nombreConNumero;
+  let i = 0
+  let nombreCompleto =`${temporalNombreConNumero}${i++}.pdf`
+
+fs.rename(PATH + nombreCortado, PATH + nombreCompleto , (err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Rename complete!");
+  });
+});
+
+
+
+}
+
+
+
+
+
+
+
+
+
